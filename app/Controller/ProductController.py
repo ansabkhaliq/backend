19d5b68f97bd3f83c914f9689c0cbfd0cf1189f0
Muhammad Ansab Khaliq <mkhaliq@student.unimelb.edu.au>
2020-09-14 20:25:11 +1000

    Marked the methods not being called by the frontend

--- app/Controller/ProductController.py
+++ app/Controller/ProductController.py
@@ -10,21 +10,6 @@ from flask import (
 
 product = Blueprint('product', __name__)
 
-@product.route('/retrieveproduct', methods=['GET'])
-def retrieve_product():
-    if not authUtil.validate_login_session():
-        return redirect(url_for('auth.login'))
-    return jsonify(product_service.retrieve_product())
-
-
-@product.route('/retrieveprice', methods=['GET'])
-def retrieve_product_price():
-    if not authUtil.validate_login_session():
-        return redirect(url_for('auth.login'))
-    return product_service.retrieve_product_price()
-
-
-
 @product.route('/api/price', methods=['POST'])
 def get_barcode_product():
     if not authUtil.validate_login_session():
@@ -35,7 +20,27 @@ def get_barcode_product():
     return jsonify(product_service.get_product_by_barcode(barcode))
 
 
+# This method is not called from the front end. These are supposed to be called by the Postman or another similar tool that
+# allow you to make calls to the REST API.
+# This method is repsonbile for getting the latest products from SQUIZZ platform and updating the table in the local database
+@product.route('/retrieveproduct', methods=['GET'])
+def retrieve_product():
+    if not authUtil.validate_login_session():
+        return redirect(url_for('auth.login'))
+    return jsonify(product_service.retrieve_product())
 
+# This method is not called from the front end. These are supposed to be called by the Postman or another similar tool that
+# allow you to make calls to the REST API.
+# This method is repsonbile for getting the latest product prices from SQUIZZ platform and updating the table in the local database
+@product.route('/retrieveprice', methods=['GET'])
+def retrieve_product_price():
+    if not authUtil.validate_login_session():
+        return redirect(url_for('auth.login'))
+    return product_service.retrieve_product_price()
+
+# This method is not called from the front end. These are supposed to be called by the Postman or another similar tool that
+# allow you to make calls to the REST API.
+# This method is repsonbile for getting the product update from SQUIZZ platform and updating the table in the local database
 @product.route('/updateproduct', methods=['GET'])
 def update_product():
     if not authUtil.validate_login_session():
@@ -43,7 +48,9 @@ def update_product():
 
     return jsonify(product_service.update_product())
 
-
+# This method is not called from the front end. These are supposed to be called by the Postman or another similar tool that
+# allow you to make calls to the REST API.
+# This method is repsonbile for getting the latest product prices from SQUIZZ platform and updating the table in the local database
 @product.route('/updateprice', methods=['GET'])
 def update_product_price():
     if not authUtil.validate_login_session():
