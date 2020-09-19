961f15b1c8d07547084e2fbf0c6825b97164f0b5
Muhammad Ansab Khaliq <mkhaliq@student.unimelb.edu.au>
2020-09-20 03:52:25 +1000

    updated the dump file

--- db/data/NewSquizz_db.sql
+++ db/data/NewSquizz_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `squizz_app` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `squizz_app`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: localhost    Database: squizz_app
@@ -442,4 +444,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-20  3:29:21
+-- Dump completed on 2020-09-20  3:34:55
